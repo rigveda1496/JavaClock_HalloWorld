e618f3ab1a444a180014ea404b7eb4c2c28fb1b1
rigveda1496 <rigveda1496@gmail.com>
2024-11-12 19:30:00 +0900

    MyFrame.java
    このクラスは、JavaのSwingライブラリを使用して、GUIウィンドウを作成するために使用します。ウィンドウには、時刻、曜日、日付がリアルタイムに更新します。

--- MyFrame.java
+++ MyFrame.java
@@ -1,3 +1,4 @@
+// インポートするライブラリ
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
@@ -7,30 +8,46 @@ import java.util.Calendar;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+// MyFrameクラス: 時計のウィンドウを表示するために使用されるクラス
 public class MyFrame extends JFrame{
  
+ // カレンダー: 日付と時間を取得するために使用されるクラス
  Calendar calendar;
+ 
+ // 日付と時間のフォーマット: SimpleDateFormatクラスを使用して日付と時間をフォーマットする
  SimpleDateFormat timeFormat;
  SimpleDateFormat dayFormat;
  SimpleDateFormat dateFormat;
+ 
+ // ラベル: 時計のウィンドウに表示されるラベル
  JLabel timeLabel;
  JLabel dayLabel;
  JLabel dateLabel;
+ 
+ // 日付と時間の文字列
  String time;
  String day;
  String date;
 
+ // MyFrameクラスのコンストラクタ: 時計のウィンドウを初期化する
  MyFrame(){
+  // ウィンドウの終了方法を設定する
   this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  // ウィンドウのタイトルを設定する
   this.setTitle("My Clock Program");
+  // ウィンドウのレイアウトを設定する
   this.setLayout(new FlowLayout());
+  // ウィンドウのサイズを設定する
   this.setSize(350,200);
+  // ウィンドウのサイズを変更できるかどうかを設定する
   this.setResizable(false);
   
+  // 日付と時間のフォーマットを設定する
   timeFormat = new SimpleDateFormat("hh:mm:ss a");
   dayFormat = new SimpleDateFormat("EEEE");
   dateFormat = new SimpleDateFormat("yyyy.MM.dd G");
   
+  // ラベルを初期化する
   timeLabel = new JLabel();
   timeLabel.setFont(new Font("",Font.PLAIN,50));
   timeLabel.setForeground(new Color(0x00FF00));
@@ -44,16 +61,22 @@ public class MyFrame extends JFrame{
   dateLabel.setFont(new Font("",Font.PLAIN,25));
   
   
+  // ラベルをウィンドウに追加する
   this.add(timeLabel);
   this.add(dayLabel);
   this.add(dateLabel);
+  // ウィンドウを表示する
   this.setVisible(true);
   
+  // 時計を更新する
   setTime();
  }
  
+ // 時計を更新するメソッド
  public void setTime() {
+  // 無限ループで時計を更新する
   while(true) {
+  // 日付と時間を取得する
   time = timeFormat.format(Calendar.getInstance().getTime());
   timeLabel.setText(time);
   
@@ -63,6 +86,7 @@ public class MyFrame extends JFrame{
   date = dateFormat.format(Calendar.getInstance().getTime());
   dateLabel.setText(date);
   
+  // 1秒待つ
   try {
    Thread.sleep(1000);
   } catch (InterruptedException e) {
